284bab7d8e651c8c8388531f545ce2e29d411f41
LeoTM <1881059+leotm@users.noreply.github.com>
2022-08-05 15:48:30 +0100

    Import template as a test

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,6 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
   singleQuote: true,
-  trailingComma: 'all',
-};
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  semi: false
+}
